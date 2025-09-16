4aba22df3ed6e9371a1d7f9f2c4125c9bcb453ae
DZhang <dingzhi.zhang@tum.de>
2025-09-16 12:05:01 +0200

    manually changed entrypoint for cb_positioning

--- cb_positioning/setup.py
+++ cb_positioning/setup.py
@@ -7,7 +7,8 @@ package_name = 'cb_positioning'
 setup(
     name=package_name,
     version='0.0.1',
-    packages=find_packages(include=[package_name, package_name + '.*']),
+    #packages=find_packages(include=[package_name, package_name + '.*']),
+    packages=[package_name],
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
