66ac829b50fa26a5219a5c7b5f8adee8b89f4f3c
ga96sew <dingzhi.zhang@tum.de>
2025-09-16 14:10:21 +0200

    update cb_humanfolow to a ros2 package

---
+++ cb_humanfollow/setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup
+from setuptools import find_packages
+import os
+from glob import glob
+package_name = 'cb_humanfollow'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    #packages=find_packages(include=[package_name, package_name + '.*']),
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='zhang',
+    maintainer_email='dingzhi.zhang@tum.de',
+    description='TODO: Package description',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'humanfollow_POS = cb_humanfollow.humanfollow_POS:main',
+        ],
+    },
+)
