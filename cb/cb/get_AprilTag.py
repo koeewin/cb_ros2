6fc29fd36d4dc452ca5efeba0ff2854df556022a
koeewin <dingzhi.zhang@tum.de>
2025-09-16 18:38:35 +0200

    align apriltag to be reviewed

--- cb/cb/get_AprilTag.py
+++ cb/cb/get_AprilTag.py
@@ -41,7 +41,7 @@ class CurrentAprilTag(Node):
         self.DIM = (960, 540)
 
         # AprilTag side length in meters
-        self.size_AprilTag = 0.062#0.1615
+        self.size_AprilTag = 0.1615#0.062#0.1615
 
         # Locate directory with calibration matrices
         path_matrices_dir = os.path.join(self.dir_ws, 'src/cb_ros2/cb/camera_calib/camera_matrices')
